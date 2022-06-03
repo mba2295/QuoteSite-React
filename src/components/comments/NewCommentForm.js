929b8272081fc8e3e884af58a55833fc3e1ac27f
Muhammad Bilal <muhammdbilalansari@gmail.com>
2022-06-03 12:09:27 +0500

    Initial commit

---
+++ src/components/comments/NewCommentForm.js
@@ -0,0 +1,44 @@
+import { useRef, useEffect } from "react";
+import useHttp from "../../hooks/use-http";
+import { addComment } from "../../lib/api";
+import Status from "../UI/Status";
+
+import classes from "./NewCommentForm.module.css";
+
+const NewCommentForm = (props) => {
+  const commentTextRef = useRef();
+  const { status, error, sendRequest } = useHttp(addComment);
+  const { onCommentAdded } = props;
+  useEffect(() => {
+    if (status === "completed" && !error) {
+      onCommentAdded();
+    }
+  }, [error, onCommentAdded, status]);
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+    // if (commentTextRef.current.value === "") {
+    //   alert("please enter some text");
+    //   return;
+    // }
+    sendRequest({
+      commentData: { text: commentTextRef.current.value },
+      quoteId: props.quoteId,
+    });
+    commentTextRef.current.value = "";
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitFormHandler}>
+      <Status status={status} error={error}></Status>
+      <div className={classes.control} onSubmit={submitFormHandler}>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
+      </div>
+      <div className={classes.actions}>
+        <button className="btn">Add Comment</button>
+      </div>
+    </form>
+  );
+};
+
+export default NewCommentForm;
