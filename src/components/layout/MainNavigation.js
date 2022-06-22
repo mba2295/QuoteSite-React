7decfb8f89e5bc8dc0e8679db9c5843cd092108b
Muhammad Bilal <muhammdbilalansari@gmail.com>
2022-06-22 20:30:52 +0500

    Update MainNavigation.js

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -3,7 +3,7 @@ import styles from "./MainNavigation.module.css";
 const MainNavigation = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>Wise Quotes</div>
+      <div className={styles.logo}>WISE-Quotes</div>
       <nav className={styles.nav}>
         <ul>
           <li>
